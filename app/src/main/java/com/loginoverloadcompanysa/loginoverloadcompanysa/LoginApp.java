acdda23668810e05dbbc9f120911feb06d40f809
SergioVelasquez <sergio-a22@hotmail.com>
2016-11-23 16:36:35 -0500

    Initial commit

---
+++ app/src/main/java/com/loginoverloadcompanysa/loginoverloadcompanysa/LoginApp.java
@@ -0,0 +1,13 @@
+package com.loginoverloadcompanysa.loginoverloadcompanysa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_app);
+    }
+}
